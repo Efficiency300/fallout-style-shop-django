ac3b528ccb9023b4901abdd9917c3cc0587e3e19
Maksud Jumaniyozov <m9989323954@gmail.com>
2026-05-18 00:13:54 +0500

    Update ALLOWED_HOSTS in settings.py
    Added allowed hosts for the Django application.

--- FallShop/settings.py
+++ FallShop/settings.py
@@ -114,7 +114,7 @@ USE_I18N = True
 
 USE_TZ = True
 
-
+ALLOWED_HOSTS = ['192.168.1.12', 'localhost', '127.0.0.1']
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
